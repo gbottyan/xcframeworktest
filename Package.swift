b5c47032c6c43d5890d224c5107608332bffcf1b
Gabor Bottyan <gabor.bottyan@accedo.tv>
2022-03-22 02:33:23 +0100

    add dependency

--- Package.swift
+++ Package.swift
@@ -15,9 +15,6 @@ let package = Package(
                         )
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-       .binaryTarget(name: "Test", path: "Sources/Test.xcframework"
-        )
-       ]
+        .binaryTarget(name: "Test", path: "Sources/Test.xcframework", dependencies: ["Promises"])
+    ]
 )
