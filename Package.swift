7ac6b64fe772175d012d21340d6fd9fd1ce1b86e
Gabor Bottyan <gabor.bottyan@accedo.tv>
2022-03-22 03:34:46 +0100

    add wrapper

--- Package.swift
+++ Package.swift
@@ -5,13 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "Test",
+    platforms: [
+          .iOS(.v10), .tvOS(.v10)
+       ],
     products: [
-        .library(name: "Test", targets: ["Test"]),
+        .library(name: "Test", targets: ["Test","TestDependencies"]),
     ],
     dependencies: [
        //.package(name: "Promises", url: "https://github.com/google/promises.git", from: "2.0.0")
     ],
     targets: [
-        .binaryTarget(name: "Test", path: "Sources/Test.xcframework" )
+        .binaryTarget(name: "Test", path: "Sources/Test.xcframework" ),
+        .target(name: "TestDependencies", dependencies: ["Promises"], path: "TestDependencies")
     ]
 )
