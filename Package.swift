a320472c095f0d3a7830073c51a987cd4d33b346
Gabor Bottyan <gabor.bottyan@accedo.tv>
2022-03-22 02:06:23 +0100

    fix package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-       .binaryTarget(name: "Test", path: "Sources/Test.xcframework" // this is a symlink
-                                            // it builds just fine in Xcode
-        ),
+       .binaryTarget(name: "Test", path: "Sources/Test.xcframework"),
+    ]
 )
