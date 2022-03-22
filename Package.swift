12ea7986c02c1b754f4895fee063e29df5db515a
Gabor Bottyan <gabor.bottyan@accedo.tv>
2022-03-22 02:56:04 +0100

    update build

--- Package.swift
+++ Package.swift
@@ -6,15 +6,12 @@ import PackageDescription
 let package = Package(
     name: "Test",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "Test",targets: ["Test"]),
+        .library(name: "Test", targets: ["Test"]),
     ],
     dependencies: [
-       .package(
-                            name: "Promises", url: "https://github.com/google/promises.git", from: "2.0.0"
-                        )
+       //.package(name: "Promises", url: "https://github.com/google/promises.git", from: "2.0.0")
     ],
     targets: [
-        .binaryTarget(name: "Test", path: "Sources/Test.xcframework", dependencies: ["Promises"])
+        .binaryTarget(name: "Test", path: "Sources/Test.xcframework" )
     ]
 )
